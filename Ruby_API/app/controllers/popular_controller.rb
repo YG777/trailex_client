c8852eb1ba07be3d65ce4e8890fa40affb3a84ba
YG777 <katyake@gamil.com>
2017-09-03 18:03:25 +0100

    popular movies working

--- Ruby_API/app/controllers/popular_controller.rb
+++ Ruby_API/app/controllers/popular_controller.rb
@@ -1,8 +1,16 @@
 class PopularController < ApplicationController
     def index
-        @movie1= Movie.new(1, "spiderman", "/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg", "2017-08-03")
-        @movie2= Movie.new(2, "spiderwoman", "/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg", "2017-08-03")
-        render json: [@movie1, @movie2]
+        # @movie1= Movie.new(1, "spiderman", "/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg", "2017-08-03")
+        # @movie2= Movie.new(2, "spiderwoman", "/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg", "2017-08-03")
+    response = HTTParty.get("https://api.themoviedb.org/3/movie/popular?api_key=83e72b5e80c8d6af6be012cfc689dfb8&language=en-US&page=1")
+   
+    movies = []
+    response["results"].each do |item|
+        movie = Movie.new(item["id"], item["title"], item["poster_path"], item["release_date"])
+
+        movies.push(movie)
+    end
+    render json: movies
     end
 end
 
@@ -14,3 +22,6 @@ class Movie
         @release_date=release_date
    end
 end
+
+
+
