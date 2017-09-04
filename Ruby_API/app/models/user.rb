cb600cd3a78539401df8f28da6647c5eaa1303fc
YG777 <katyake@gamil.com>
2017-09-04 23:06:01 +0100

    rails authentications controller and routes

--- Ruby_API/app/models/user.rb
+++ Ruby_API/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :username, presence: true
+    validates :email, uniqueness: true, presence: true
     has_one :watchlist
     has_many :comments
 end
--- Ruby_API/config/routes.rb
+++ Ruby_API/config/routes.rb
@@ -1,9 +1,13 @@
 
 Rails.application.routes.draw do
+ scope :api do
   resources :comments
   resources :watchlists
   resources :movies
-  resources :users
   resources :popular
   resources :search
+  resources :users, except: [:create]
+  post "register", to: "authentications#register"
+  post "login", to: "authentications#login"
+ end
 end
