5f2158f63a3872b68350793681112134e2ada43a
YG777 <katyake@gamil.com>
2017-09-06 22:19:50 +0100

    added videos to returned data

--- Ruby_API/app/controllers/movie_controller.rb
+++ Ruby_API/app/controllers/movie_controller.rb
@@ -1,16 +1,31 @@
 class MovieController < ApplicationController
   def show
     response = HTTParty.get("https://api.themoviedb.org/3/movie/#{params[:id]}?api_key=83e72b5e80c8d6af6be012cfc689dfb8&append_to_response=videos")
-    showMovie = ShowMovie.new(response["id"], response["title"], response["poster_path"], response["release_date"])
+    videoKeys = []
+
+    response["videos"]["results"].each do |video_details|
+      if video_details["site"] == "YouTube"
+        key = video_details["key"]
+        videoKeys.push(key)
+      end
+    end
+
+    showMovie = ShowMovie.new(
+      response["id"], 
+      response["title"], 
+      response["poster_path"], 
+      response["release_date"],
+      videoKeys)
     render json: showMovie
   end
 end
 
 class ShowMovie
-  def initialize(id, title, poster, releaseDate)
+  def initialize(id, title, poster, release_date, video_keys)
     @id = id
     @title = title
     @poster = poster
-    @releaseDate = releaseDate
+    @release_date = release_date
+    @video_keys = video_keys
   end
 end
