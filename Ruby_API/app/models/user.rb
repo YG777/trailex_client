3def327648b849b7ee0047249661d9f10ef077bc
YG777 <katyake@gamil.com>
2017-10-08 12:12:48 +0100

    removing api code

--- Ruby_API/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-    has_secure_password
-    validates :username, :email , presence: true, uniqueness: true 
-    has_many :watchlist
-    has_many :comments
-end
--- Ruby_API/app/serializers/comment_serializer.rb
+++
@@ -1,3 +0,0 @@
-class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :movie_id, :body
-end
